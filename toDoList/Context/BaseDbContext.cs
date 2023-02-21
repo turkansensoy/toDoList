728dd7f2577fd87f8c18fd8c6b42010304218bc3
türkan şensoy <trksnsy01@gmail.com>
2023-02-21 09:37:44 +0300

    Add project files.

---
+++ toDoList/Context/BaseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Net.NetworkInformation;
+using toDoList.Data.Models;
+
+namespace toDoList.Context
+{
+    public class BaseDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<ThingstoDo> thingstoDo { get; set; }
+        public DbSet<BeingDone> beingDone { get; set; }
+        public DbSet<Done> done { get; set; }
+
+    }
+}
---
+++ toDoList/Data/Services/BeginDoneService.cs
@@ -0,0 +1,19 @@
+﻿using toDoList.Context;
+using toDoList.Data.Models;
+
+namespace toDoList.Data.Services
+{
+    public class BeginDoneService
+    {
+        private readonly BaseDbContext _context;
+        public BeginDoneService(BaseDbContext context)
+        {
+            _context = context;
+        }
+        public List<BeingDone> GetAll()
+        {
+            var result = _context.beingDone.ToList();
+            return result;
+        }
+    }
+}
